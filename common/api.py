534889fa38219db6f2ba7151e075efd82db94c98
宋国雄 <489406796@qq.com>
2019-07-22 22:59:53 +0800

    bug fixed

--- common/api.py
+++ common/api.py
@@ -3,5 +3,6 @@ import requests
 
 def online_encrypt(url: str, path: str, request_args: dict, **kwargs) -> dict:
     """调用api加密"""
-    response = requests.post(url, json=dict(path=path, request_args=request_args, **kwargs))
-    return response.json()
+    response = requests.post(url, json=dict(path=path, request_args=request_args), **kwargs)
+    json_data = response.json()
+    return json_data
