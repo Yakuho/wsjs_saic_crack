f3ac60cae49cdd9959c095df8f754cf16eb25d47
宋国雄 <489406796@qq.com>
2019-07-22 23:40:38 +0800

    更详细的类型注释

--- common/api.py
+++ common/api.py
@@ -1,7 +1,9 @@
+from typing import Dict, Union
+
 import requests
 
 
-def online_encrypt(url: str, path: str, request_args: dict, **kwargs) -> dict:
+def online_encrypt(url: str, path: str, request_args: Dict[str, Union[str, int]], **kwargs) -> dict:
     """调用api加密"""
     response = requests.post(url, json=dict(path=path, request_args=request_args), **kwargs)
     json_data = response.json()
